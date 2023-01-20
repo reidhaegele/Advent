73fe8fc047a364f8d66491752e9492f4e831d804
Reid Haegele <rahdb4@umsystem.edu>
2023-01-19 22:19:07 -0600

    Advent day one part two complete!

---
+++ advent2.py
@@ -0,0 +1,31 @@
+f = open("input.txt", "r")
+max = 0;
+second_max = 0;
+third_max = 0;
+sum = 0;
+for x in f:
+  if(x=="\n"):
+    if(sum > max):
+      if (max>second_max):
+        if(second_max > third_max):
+          third_max=second_max;
+        second_max=max;
+      max=sum;
+    else:
+      if(sum > second_max):
+        if (max>second_max):
+          third_max=second_max;
+        second_max = sum;
+      else:
+        if(sum > third_max):
+          third_max = sum;
+    sum = 0;
+  else:
+    sum += int(x);
+f.close()
+total = max+second_max+third_max
+print(total)
+print("MAX:"+(str(max)))
+print("SECOND MAX:"+(str(second_max)))
+print("THIRD MAX:"+(str(third_max)))
+print("TOTAL:"+(str((max+second_max+third_max))))
