dc2dd14855cb9ae4561f93480115bb7ffbd92fdc
Reid Haegele <rahdb4@umsystem.edu>
2023-01-23 11:02:04 -0600

    Clean up day six part one solution

--- DaySix/subroutine.py
+++ DaySix/subroutine.py
@@ -1,15 +1,10 @@
 f = open("inputDay6.txt", "r")
-one   = ''
-two   = ''
-three = ''
-four  = ''
 fourLetters = []
 success = False
 result = 0
 for x in f:
         for char in x:
                 result+=1
-                print(result)
                 if(len(fourLetters) < 4):
                         fourLetters.append(char)
                 else:
@@ -25,9 +20,7 @@ for x in f:
                         if(len(set(fourLetters)) == len(fourLetters)): 
                                 success = True
                                 break
-                if(success): break
         if(success): break
 
-print("-=-=-=-=-")
 print(result)
 f.close()
